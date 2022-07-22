dbffa8b40cdbc989cfb098d121546d43ec3938ad
Dini Abdiu <diniabdiu@gmail.com>
2022-07-22 11:46:31 +0200

    new build & fixed endpoints with mongodb

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 
 const uuid = require('uuid')
+const uuidv4 = uuid.v4
 
 const Note = require('./models/note')
 
@@ -18,40 +19,48 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
-    response.json(notes)
+    response.json(notes).end()
   })
 })
 
 app.get('/api/notes/:id', (req, res) => {
   const id = Number(req.params.id)
-  const note  = notes.find(note => note.id === id)
-  res.json(note).end()
+
+  Note.findById(id).then(note => {
+    res.json(note).end()
+  })  
 })
 
 
 
 app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  notes = notes.find(note => note.id === id)
-  res.status(204).end()
+  const id = req.params.id
+  // notes = notes.find(note => note.id === id)
+  Note.findByIdAndRemove(id)
+  .then(result => {
+    res.status(204).end()
+  })
+  
 })
 
 app.post('/api/notes', (req,res) => {
   const body = req.body
-  console.log(body)
+  
   if (body.content === undefined) {
-    return res.status(400).json({ error: 'content missing' })
+    return res.status(400).json({ error: 'content missing' }).end()
   }
 
-  const note = {
+  const note = new Note( {
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: uuid
-  }
+    id: uuidv4()
+  })
 
-  
-    res.json(note).end()
+  note.save().then(savedNote => {
+    console.log(savedNote)
+    res.json(savedNote).end()
+  })
  
 })
 
@@ -61,11 +70,13 @@ app.put('/api/notes/:id', (req, res) => {
   const body = req.body
   // find id from params and convert to number for sure!
   const id = Number(req.params.id)
+  Note.findByIdAndUpdate(id, body).then(noteUpdate => {
+    res.json(noteUpdate).end()
+  })
+  // const noteIndex = notes.findIndex(n => n.id === id)
 
-  const noteIndex = notes.findIndex(n => n.id === id)
-
-  notes[noteIndex] = body
-  res.json(notes[noteIndex]).end()
+  // notes[noteIndex] = body
+  // res.json(notes[noteIndex]).end()
 })
 const PORT = process.env.PORT
 app.listen(PORT, () => {
